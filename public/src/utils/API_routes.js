6a61fa72904c1d3ce7975ad1ab17c4c5a8d16513
Rhitam-Banerjee <banerjeerhitam1@gmail.com>
2024-01-01 15:39:01 +0530

    vercelChanges

--- public/src/utils/API_routes.js
+++ public/src/utils/API_routes.js
@@ -1,5 +1,5 @@
-// const host = "https://conext-server.vercel.app";
-const host = "http://localhost:5000";
+const host = "https://conext-server.vercel.app";
+// const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/api/auth", userRoutes);
 mongoose
