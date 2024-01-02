6ef3c5572294c1885c6f68979a5a7842109ea53f
Rhitam-Banerjee <banerjeerhitam1@gmail.com>
2024-01-02 13:14:57 +0530

    CHECK:minorChangesRender4

--- public/src/utils/API_routes.js
+++ public/src/utils/API_routes.js
@@ -1,5 +1,6 @@
 // const host = "https://conext-server.vercel.app";
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://conext-server.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
